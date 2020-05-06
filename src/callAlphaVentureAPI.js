eba372804bfe8f405ee21220c02348bc520eb30c
Charles Wingstrom <charliewingstrom@yahoo.com>
2020-05-05 21:02:41 -0500

    created a basic chart from alphaVenture API

--- src/callAlphaVentureAPI.js
+++ src/callAlphaVentureAPI.js
@@ -1,6 +1,8 @@
 
  export default
  function callAlphaVenture(stockCode, timeFrame) {
-    fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockCode}&interval=${timeFrame}&apikey=2105D1X8XZN7K357`)
-    .then(res => console.log(res.json()))
+    return fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockCode}&apikey=2105D1X8XZN7K357`)
+    .then(res => {
+        return res.json()
+    });
 }
--- src/page.js
+++ src/page.js
@@ -1,15 +1,78 @@
 import React from 'react';
 import callAlphaVenture from './callAlphaVentureAPI.js';
+import Chart from '../node_modules/chart.js/dist/Chart.bundle.js';
+
+
 
 export default 
 class Page extends React.Component {
-    constructor(props) {
-        super(props);
-       callAlphaVenture('IBM', '5min')
+
+    chartRef = React.createRef();
+
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+
+        var tmp = callAlphaVenture('IBM', '5min');
+        tmp.then(data => {
+            var chartData = data['Time Series (Daily)'];
+            
+            var dates = [];
+            var openingValues = []
+            for (const [key, value] of Object.entries(chartData)) {
+                dates.push(key);
+                openingValues.push(value['1. open']);
+            }
+            console.log(dates);
+            console.log(openingValues);
+
+            return [dates, openingValues]
+        })
+        .then(chartDataArray => {
+            new Chart(myChartRef, {
+                type: 'line',
+                data: {
+                    labels: chartDataArray[0],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: chartDataArray[1],
+                        backgroundColor: 
+                            'rgba(255, 99, 132, 0.2)',
+                        
+                        borderColor: 
+                            'rgba(255, 99, 132, 1)',
+
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+
+        })
+
+        .catch(err => console.log("there was an error: " + err));
+
+        
     }
     render() {
+        
         return (
-            <h1>This is a starter page.</h1>
+            <div className={'page'}>
+                <h1>This is a starter page.</h1>
+                <canvas 
+                    id="myChart" 
+                    width="400" 
+                    height="400"
+                    ref={this.chartRef}
+                />
+            </div>
         )
     }
 }
