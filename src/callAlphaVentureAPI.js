d85be437ede2ffc4744f19936306a910142b1cf7
Charles Wingstrom <charliewingstrom@yahoo.com>
2020-05-20 13:36:22 -0500

    started adding functionality where the user can select a time stamp

--- src/callAlphaVentureAPI.js
+++ src/callAlphaVentureAPI.js
@@ -1,7 +1,7 @@
 
  export default
  function callAlphaVenture(stockCode, timeFrame) {
-    return fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockCode}&apikey=2105D1X8XZN7K357`)
+    return fetch(`https://www.alphavantage.co/query?function=${timeFrame}&symbol=${stockCode}&apikey=2105D1X8XZN7K357`)
     .then(res => {
         return res.json()
     });
--- src/page.js
+++ src/page.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import callAlphaVenture from './callAlphaVentureAPI.js';
 import Chart from '../node_modules/chart.js/dist/Chart.bundle.js';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 export default 
 class Page extends React.Component {
@@ -8,20 +13,24 @@ class Page extends React.Component {
         super(props);
         this.state = {
             stockCode: 'AAPL',
+            timeSeries: 'TIME_SERIES_DAILY',
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.changeTimeSeries = this.changeTimeSeries.bind(this);
     }
-    
+    timeStampData = 
+        {'TIME_SERIES_DAILY': 'Time Series (Daily)', 
+        'TIME_SERIES_WEEKLY': 'Weekly Time Series' }
     chartRef = React.createRef();
 
     componentDidMount() {
         const myChartRef = this.chartRef.current.getContext("2d");
        
-        var tmp = callAlphaVenture(this.state.stockCode, '5min');
+        var tmp = callAlphaVenture(this.state.stockCode, this.state.timeSeries);
         tmp.then(data => {
-            var chartData = data['Time Series (Daily)'];
-            
+            var chartData = data[this.timeStampData[this.state.timeSeries]];
+            console.log(data);
             var dates = [];
             var openingValues = []
             for (const [key, value] of Object.entries(chartData)) {
@@ -73,18 +82,56 @@ class Page extends React.Component {
         event.preventDefault();
         this.componentDidMount();
     }
+    changeTimeSeries(event) {
+        console.log("The current target value is " + event.currentTarget.value);
+        this.setState({
+            timeSeries: event.currentTarget.value,
+        })
+    }
     render() {
         return (
             <div className={'page'}>
                 <h1>This is a starter page.</h1>
                 <h3>The stock is {this.state.stockCode}</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Enter a Code:
-                        <input type="text" value={this.state.stockCode} onChange={this.handleChange} />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
+                <div className={'chartHeader'}>
+                    <div className={'headerItem'}>
+                        <form onSubmit={this.handleSubmit}>
+                            <label>
+                                Enter a Code:<br/>
+                                <input type="text" value={this.state.stockCode} onChange={this.handleChange} />
+                            </label>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </div>
+                    <div className={'headerItem'}>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">Time Series</FormLabel>
+                            <RadioGroup row aria-label="position" name="position" defaultValue="top">
+                                <FormControlLabel
+                                value="TIME_SERIES_DAILY"
+                                control={<Radio color="primary" />}
+                                label="Daily"
+                                labelPlacement="top"
+                                onChange={this.changeTimeSeries}
+                                />
+                                <FormControlLabel
+                                value="TIME_SERIES_WEEKLY"
+                                control={<Radio color="primary" />}
+                                label="Weekly"
+                                labelPlacement="top"
+                                onChange={this.changeTimeSeries}
+                                />
+                                <FormControlLabel
+                                value="TIME_SERIES_MONTHLY"
+                                control={<Radio color="primary" />}
+                                label="Monthly"
+                                labelPlacement="top"
+                                onChange={this.changeTimeSeries}
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </div>
+                </div>
                 <canvas 
                     id="myChart" 
                     width="400" 
