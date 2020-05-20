39194a8f9af7036b202cf701aa8ef57b549d6fef
Charles Wingstrom <charliewingstrom@yahoo.com>
2020-05-19 22:41:29 -0500

    allow user to enter a stock code

--- src/page.js
+++ src/page.js
@@ -2,17 +2,23 @@ import React from 'react';
 import callAlphaVenture from './callAlphaVentureAPI.js';
 import Chart from '../node_modules/chart.js/dist/Chart.bundle.js';
 
-
-
 export default 
 class Page extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            stockCode: 'AAPL',
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
     chartRef = React.createRef();
 
     componentDidMount() {
         const myChartRef = this.chartRef.current.getContext("2d");
-
-        var tmp = callAlphaVenture('IBM', '5min');
+       
+        var tmp = callAlphaVenture(this.state.stockCode, '5min');
         tmp.then(data => {
             var chartData = data['Time Series (Daily)'];
             
@@ -20,6 +26,7 @@ class Page extends React.Component {
             var openingValues = []
             for (const [key, value] of Object.entries(chartData)) {
                 dates.push(key);
+                // getting the opening value for the day
                 openingValues.push(value['1. open']);
             }
             console.log(dates);
@@ -31,10 +38,10 @@ class Page extends React.Component {
             new Chart(myChartRef, {
                 type: 'line',
                 data: {
-                    labels: chartDataArray[0],
+                    labels: chartDataArray[0].reverse(),
                     datasets: [{
                         label: '# of Votes',
-                        data: chartDataArray[1],
+                        data: chartDataArray[1].reverse(),
                         backgroundColor: 
                             'rgba(255, 99, 132, 0.2)',
                         
@@ -54,18 +61,30 @@ class Page extends React.Component {
                     }
                 }
             });
-
         })
-
         .catch(err => console.log("there was an error: " + err));
+    }
 
-        
+    handleChange(event) {
+        this.setState({stockCode: event.target.value});
+      }
+    handleSubmit(event) {
+        event.preventDefault();
+        this.setState({stockCode: event.target.value});
+        this.componentDidMount();
     }
     render() {
-        
         return (
             <div className={'page'}>
                 <h1>This is a starter page.</h1>
+                <h3>The stock is {this.state.stockCode}</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Enter a Code:
+                        <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
                 <canvas 
                     id="myChart" 
                     width="400" 
