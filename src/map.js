87eccd9e1ddbbba1b05c81bbb72c1456551c7a4e
jpkravcik <jpkravcik@gmail.com>
2020-05-27 23:32:15 +0100

    Fixed callCovidTrackingAPI file name

--- src/map.js
+++ src/map.js
@@ -3,7 +3,7 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import am4geodata_usaLow from "@amcharts/amcharts4-geodata/usaLow";
-import callCovidTracking from "./callCovidTrackingApi.js";
+import callCovidTracking from "./callCovidTrackingAPI.js";
 
 
 export default class Map extends React.Component {
