16e39f262d6f2b9a14f53721c6a0608327bec6f3
HolmesA10 <46690516+HolmesA10@users.noreply.github.com>
2020-05-27 20:59:58 -0500

    added routing and additional pages
    separate welcome page, graph ,chart, and about page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app.js';
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.render(
     //<Page/>,
     //<Map/>,
-    <App/>,
+    <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   	document.getElementById('root')
 );
 
